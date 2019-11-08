fe45fcd4698d9ec7b69c604b973eeadd31efc17c
cfowler650 <cfowler650@gmail.com>
2019-11-08 11:02:30 -0600

    styling

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -23,6 +23,27 @@ class Main extends React.Component {
                                     <div className="slide-header">
                                         <h1>About</h1>
                                     </div>
+                                    <p className="text">
+                                        I'm a web developer with experience in front end and back end technologies.
+                                        I fell in love with programming at a young age and have aspired to do it professionally ever since.
+                                        My favorite library right now is React, I love developing in react and mobile development with
+                                        React Native excites me. I have built and used many REST APIs however I'm really inspired by
+                                        the switch to GraphQL and all that it has taught me.
+                                    </p>
+
+                                    <div>
+                                        <h2 style={{
+                                            margin: "1em 0px",
+                                            textTransform: "uppercase",
+                                            letterSpacing: "0.5em", fontFamily: "nunito-extrabold",
+                                        }}>Skills</h2>
+                                        <ul style={{ paddingLeft: "20px" }}>
+                                            <li>React</li>
+                                            <li>Ruby on Rails</li>
+                                            <li>REST</li>
+                                            <li>GraphQL</li>
+                                        </ul>
+                                    </div>
                                 </div>
                             </div>
                         </section>
@@ -31,6 +52,29 @@ class Main extends React.Component {
                                 <div className="slide-contents-inner">
                                     <div className="slide-header">
                                         <h1>Projects</h1>
+                                    </div>
+                                    <div className="project-div">
+                                        <div className="project-title">
+                                            <h1>Belch</h1>
+                                        </div>
+
+                                        <div className="project-contents">
+                                            <div className="img-container">
+                                                <img className="project-img" src="belchscreen.png" />
+
+                                            </div>
+
+                                            <div className="project-description">
+                                                <p>
+                                                    This project means a lot to me, yada, it is bascially
+                                                    a really cool copy of yelp but with dish reviews instead of
+                                                    restaurants
+                                                </p>
+                                                <p>
+                                                    <a>Github</a>
+                                                </p>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
