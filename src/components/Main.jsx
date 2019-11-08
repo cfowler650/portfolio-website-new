afe78a383d60e477ab75ed7b406b0878273ca50b
cfowler650 <cfowler650@gmail.com>
2019-11-08 09:43:01 -0600

    add snap slides

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -13,10 +13,38 @@ class Main extends React.Component {
                                 <h1 className="name-header">Caleb Fowler</h1>
                                 <p className="sub-header">New York, New York</p>
                             </div>
-
                         </div>
                     </div>
 
+                    <div class="slide-container">
+                        <section class="slide">
+                            <div className="slide-contents">
+                                <div className="slide-contents-inner">
+                                    <div className="slide-header">
+                                        <h1>About</h1>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                        <section class="slide">
+                            <div className="slide-contents">
+                                <div className="slide-contents-inner">
+                                    <div className="slide-header">
+                                        <h1>Projects</h1>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                        <section class="slide">
+                            <div className="slide-contents">
+                                <div className="slide-contents-inner">
+                                    <div className="slide-header">
+                                        <h1>Resume</h1>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
                 </div>
             </div>
         )
